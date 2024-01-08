b08b6a9dde6af87787bd73750addae870969f241
KaungZawHtetViatick <124129191+KaungZawHtetViatick@users.noreply.github.com>
2024-01-09 01:59:13 +0700

    feature : app bar improved

--- frontend/app/components/responsive-app-bar.tsx
+++ frontend/app/components/responsive-app-bar.tsx
@@ -18,25 +18,13 @@ import Web3 from 'web3';
 
 declare var window: any
 
-const pages = ['Products', 'Pricing', 'Blog'];
+//const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 let web3;
 function ResponsiveAppBar() {
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
     const [error, setError] = React.useState<null | HTMLElement>(null);
 
-
-
-
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget);
-    };
-
-
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
 
 
 
@@ -94,42 +82,7 @@ function ResponsiveAppBar() {
                         My Vending Machine
                     </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                    <Typography textAlign="center">{page}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
+
                     <PointOfSale sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
                     <Typography
                         variant="h5"
@@ -149,15 +102,7 @@ function ResponsiveAppBar() {
                         My Mach
                     </Typography>
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                key={page}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page}
-                            </Button>
-                        ))}
+
                     </Box>
 
                     <Box sx={{ flexGrow: 0 }}>
